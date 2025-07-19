71bdd527394bb09a3515e8e2fb496be81c4911ec
ShinySilver <nicolas.louismarie@gmail.com>
2025-07-19 19:28:18 +0200

    Removed SPIRV-Tools Optimizer dependency

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 use std::{env, process};
 use std::fs;
 use std::path::{Path, PathBuf};
-use spirv_tools::opt::Optimizer;
 use walkdir::WalkDir;
 
 fn main() {
@@ -41,17 +40,9 @@ fn main() {
                 process::exit(1);
             });
 
-        // 2) SPIR-V → optimized SPIR-V
-        let mut optimizer = spirv_tools::opt::compiled::CompiledOptimizer::default();
-        optimizer.register_performance_passes();
-        let optimized_binary = optimizer.optimize(compiled.as_binary(), &mut |msg| {
-            eprintln!("[compiled] optimizer message: {:#?}", msg);
-        }, None).expect("Could not optimize shader.");
-
         // 3) write out the optimized shader
         let spv_path = out_dir.join(format!("{filename}.spv"));
-        fs::write(&spv_path, optimized_binary.as_bytes()).expect("Failed to write optimized SPIR-V");
-        //fs::write(&spv_path, compiled.as_binary_u8()).expect("Failed to write optimized SPIR-V");
+        fs::write(&spv_path, compiled.as_binary_u8()).expect("Failed to write SPIR-V code");
 
         println!("cargo:rerun-if-changed={}", path.display());
     }
